c8eb433f97f1ee084969edd5008d6f562cb0a26a
victoralvesf <viictor.alvess12@gmail.com>
2021-12-30 00:06:11 -0300

    add more jokes and queue to not repeat

--- src/app/JokeController.js
+++ src/app/JokeController.js
@@ -1,12 +1,19 @@
-import _ from 'lodash'
-
 import { jokes } from '../mock/jokes'
+
+let counter = 0
+let actual = 0
 
 class JokeController {
   index(req, res) {
-    const randomJoke = _.sample(jokes)
-
-    return res.send(randomJoke)
+    if (counter > jokes.length - 1) {
+      actual = 0
+      counter = actual + 1
+      return res.send(jokes[actual])
+    } else {
+      actual = counter
+      counter += 1
+      return res.send(jokes[actual])
+    }
   }
 }
 
